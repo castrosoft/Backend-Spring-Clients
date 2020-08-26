8905c9d18903d194208624728c61142147543940
Piter Castro <castro.fcefyn@gmail.com>
2020-08-26 00:47:04 -0300

    agrego la anotacion de @CrossOrigin en el controller

--- src/main/java/com/mybusiness/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/mybusiness/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -3,6 +3,7 @@ package com.mybusiness.springboot.backend.apirest.controllers;
 import com.mybusiness.springboot.backend.apirest.models.entity.Cliente;
 import com.mybusiness.springboot.backend.apirest.models.services.ClienteServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,8 @@ import java.util.List;
  * Esta es mi API Rest
  */
 
+//Implemento CORS. Doy acceso a este dominio (Front de Angular) para que pueda enviar/recibir datos. Por default, permito todo
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 //Genero la url (el endpoint)
 @RequestMapping("/api")
