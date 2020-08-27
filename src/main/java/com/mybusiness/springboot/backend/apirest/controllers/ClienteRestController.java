067bf05d54b82e50805c1a1ce5f2ea861d17c095
Piter Castro <castro.fcefyn@gmail.com>
2020-08-26 21:19:44 -0300

    Agrego PUT y DELETE en el controller

--- src/main/java/com/mybusiness/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/mybusiness/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -42,4 +42,22 @@ public class ClienteRestController {
     public Cliente create(@RequestBody Cliente cliente){
         return clienteService.save(cliente);
     }
+
+    @PutMapping("/clientes/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id){
+        Cliente clienteActual = clienteService.findById(id);
+
+        clienteActual.setApellido(cliente.getApellido());
+        clienteActual.setNombre(cliente.getNombre());
+        clienteActual.setEmail(cliente.getEmail());
+
+        return clienteService.save(clienteActual);
+    }
+
+    @DeleteMapping("/clientes/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id){
+        clienteService.delete(id);
+    }
 }
