373c1b681bf63e970617a865dd951c1834a2b52f
Piter Castro <castro.fcefyn@gmail.com>
2020-11-23 17:02:45 -0300

    Agrego Swagger 2.9.2. Aun no tiene la version grafica ui. Solo se ve en un json en /v2/api-docs

--- src/main/java/com/mybusiness/springboot/backend/apirest/config/AppConfig.java
+++ src/main/java/com/mybusiness/springboot/backend/apirest/config/AppConfig.java
@@ -5,15 +5,31 @@ import com.mybusiness.springboot.backend.apirest.models.services.ClienteService;
 import com.mybusiness.springboot.backend.apirest.models.services.ClienteServiceInterface;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.RequestHandler;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.List;
 
 @Configuration
+@EnableSwagger2
 public class AppConfig {
 
     @Bean
-    public ClienteServiceInterface createClienteServiceInterface(){
+    public ClienteServiceInterface createClienteServiceInterface() {
         return new ClienteService();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
     }
 
 }
